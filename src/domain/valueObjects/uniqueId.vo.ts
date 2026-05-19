aaa7e9baff0f39fc56d32a941f603ce2487aa409
nykt3r <juan.algz02@gmail.com>
2026-05-19 13:36:04 -0500

    fix: uniqueid typed as uuid value

--- src/domain/valueObjects/uniqueId.vo.ts
+++ src/domain/valueObjects/uniqueId.vo.ts
@@ -1,13 +1,15 @@
 import { randomUUID } from "node:crypto";
 
-export class UniqueId {
-  private readonly value: string;
+type UUID = `${string}-${string}-${string}-${string}-${string}`;
 
-  constructor(value?: string) {
+export class UniqueId {
+  private readonly value: UUID;
+
+  constructor(value?: UUID) {
     this.value = value ?? randomUUID();
   }
 
-  getValue(): string {
+  getValue(): UUID {
     return this.value;
   }
 }
